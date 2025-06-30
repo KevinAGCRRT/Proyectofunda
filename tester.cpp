cc39e9b1ba34b2d0f7c67d6cbe29a35cc71bf3e4
KevinAGCRRT <00026825@uca.edu.sv>
2025-06-29 18:22:32 -0600

    actualizado

--- tester.cpp
+++ tester.cpp
@@ -23,7 +23,7 @@ int main()
         {"Que fecha celebra El Salvador como Independencia?", {"A) 5 de mayo de 1821", "B) 15 de septiembre de 1821", "C) 20 de noviembre de 1821"}, 'B'},
         {"Que pueblos indigenas vivieron en territorio salvadoreno antes de la llegada de los espanoles?", {"A) Mayas y Pipiles", "B) Incas y Chibchas", "C) Nahuas y Quechuas"}, 'A'},
         {"Quien fue el primer presidente de El Salvador?", {"A) Francisco Morazan", "B) Manuel Jose Arce", "C) Gerardo Barrios"}, 'B'},
-        {"Que guerra vivio El Salvador en la decada de 1980?", {"A) La guerra de los 100 años", "B) La guerra civil salvadoreña", "C) La guerra de Centroamerica"}, 'B'},
+        {"Que guerra vivio El Salvador en la decada de 1980?", {"A) La guerra de los 100 anos", "B) La guerra civil salvadorena", "C) La guerra de Centroamerica"}, 'B'},
         {"Que acuerdo de paz puso fin a la guerra civil en El Salvador?", {"A) Tratado de Versalles", "B) Acuerdo de Camp David", "C) Acuerdos de Chapultepec"}, 'C'},
         {"Que heroe nacional salvadoreño lidero la lucha en el levantamiento de 1932?", {"A) Feliciano Ama", "B) Anastasio Aquino", "C) José Simeon Cañas"}, 'A'},
         {"Que evento comenzo en 1914?", {"A) La Revolución Francesa", "B) La Primera Guerra Mundial", "C) La Guerra Fria"}, 'B'},
@@ -36,17 +36,49 @@ int main()
         {"Que banda britanica estaba compuesta por John Lennon, Paul McCartney, George Harrison y Ringo Starr?", {"A) Queen", "B) The Beatles", "C) The Rolling Stones"}, 'B'},
         {"Quien canta WHITE FERRARY?", {"A) Kanye West", "B) Franl Ocean", "C) Bad Bunny"}, 'B'},
         {"Que genero musical nacio en el Bronx, Nueva York, en los anos 1970?", {"A) Jazz", "B) Rock and Roll", "C) Hip Hop"}, 'C'},
-        {"Que ciudad de Estados Unidos es conocida como la cuna del Hip Hop?", {"A) Los Ángeles", "B) Atlanta", "C) Nueva York (Bronx)"}, 'C'},
+        {"Que ciudad de Estados Unidos es conocida como la cuna del Hip Hop?", {"A) Los Angeles", "B) Atlanta", "C) Nueva York (Bronx)"}, 'C'},
         {"Que artista latino tiene exitos como Vivir Mi Vida?", {"A) Marc Anthony", "B) Maluma", "C) Romeo Santos"}, 'A'},
         {"Que estilo musical popular nacio en Republica Dominicana?", {"A) Cumbia", "B) Merengue", "C) Tango"}, 'B'},
         {"Que artista latino es reconocido como pionero del trap en espanol?", {"A) Bad Bunny", "B) Anuel AA", "C) Arcángel"}, 'A'},
-        {"Que banda de rock en espanol es de Argentina?", {"A) Soda Stereo", "B) Maná", "C) Café Tacvba"}, 'A'},
-        {"Que cantante urbana canta Saoko?", {"A) Karol G", "B) Bad Bunny", "C) Rosalía"}, 'C'},
+        {"Que banda de rock en espanol es de Argentina?", {"A) Soda Stereo", "B) Mana", "C) Cafe Tacvba"}, 'A'},
+        {"Que cantante urbana canta Saoko?", {"A) Karol G", "B) Bad Bunny", "C) Rosalia"}, 'C'},
         {"Que grupo de Memphis es considerado pionero en el estilo Memphis Rap?", {"A) Three 6 Mafia", "B) Outkast", "C) Run-D.M.C"}, 'A'},
         {"Que artista canta Bichota?", {"A) Karol G", "B) Natti Natasha", "C) Anuel AA"}, 'A'},
         {"Que género mezcla el jazz, musica afroamericana y latina?", {"A) Soul", "B) Latin Jazz", "C) Reggae"}, 'B'},
         {"Que cantante es conocido como por sus comentarios polemicos en X?", {"A) Ye", "B) Kanye west", "C) Todas son correctas"}, 'C'},
         {"Que artista tiene el estilo de musica urbana en exitos como Felices los 4?", {"A) Maluma", "B) J Balvin", "C) Ozuna"}, 'A'}};
+        vector<pregunta> preguntad ={
+        {"Cuantos jugadores hay en el campo por equipo en un partido de futbol?", {"a) 9", "b) 10", "c) 11"}, 'C'},
+        {"Cada cuantos anos se celebra la Copa Mundial de la FIFA?",{"a) Cada 2 anos", "b) Cada 4 anos", "c) Cada 5 anos"}, 'B'},
+        {"Que pais ha ganado mas Copas del Mundo?",{"a) Alemania", "b) Italia", "c) Brasil"}, 'C'},
+        {"Como se llama el torneo de clubes mas importante de Europa?", {"a) Europa League", "b) Champions League", "c) Conference League"}, 'B'},
+        {"Cual de estos paises nunca ha ganado un Mundial?",{"a) Inglaterra", "b) Espana", "c) Paises Bajos"}, 'C'},
+        {"Que significa 'hat-trick' en futbol?",{"a) Anotar tres goles en un partido", "b) Ganar tres titulos en un ano", "c) Defender tres penales"}, 'A'},
+        {"Que pais fue sede del Mundial en 2022?", {"a) Rusia", "b) Qatar", "c) Japon"}, 'B'},
+        {"Cual es la maxima autoridad en un partido de futbol?", {"a) El entrenador", "b) El capitan", "c) El arbitro"}, 'C'},
+        {"Que superficie es la mas comun en el futbol profesional?",{"a) Arena", "b) Cesped", "c) Concreto"}, 'B'},
+        {"Que pasa si un partido de eliminacion directa termina empatado tras los 90 minutos y el tiempo extra?",{"a) Se repite el partido", "b) Se define por penales", "c) Se da empate"}, 'B'},
+        {"Cuantos anillos tiene el simbolo olimpico?", {"a) 4", "b) 5", "c) 6"}, 'B'},
+        {"Que deporte se juega en un campo llamado 'diamond' en ingles?", {"a) Rugby", "b) Hockey", "c) Beisbol"}, 'C'},
+        {"Cual es el principal torneo anual de tenis sobre cesped?", {"a) Roland Garros", "b) Wimbledon", "c) US Open"}, 'B'},
+        {"Cuantos sets se necesitan ganar para triunfar en un partido de tenis masculino de Grand Slam?",{"a) 2", "b) 3", "c) 4"}, 'B'},
+        {"Que deporte usa un disco llamado 'puck'?",{"a) Balonmano", "b) Hockey sobre hielo", "c) Waterpolo"}, 'B'}};
+        vector<pregunta> preguntag ={
+        {"Cual es el rio mas largo del mundo?", {"a) Amazonas", "b) Nilo", "c) Yangtse"}, 'A'},
+        {"Cual es la capital de Suecia?", {"a) Copenhague", "b) Estocolmo", "c) Oslo"}, 'B'},
+        {"En que continente se encuentra Vietnam?",{"a) Asia", "b) Europa", "c) Africa"}, 'A'},
+        {"Cual es el pais con mas islas en el mundo?", {"a) Indonesia", "b) Suecia", "c) Filipinas"}, 'B'},
+        {"Cual es el desierto mas grande del mundo?",{"a) Sahara", "b) Antartico", "c) Gobi"}, 'B'},
+        {"Cual es la montana mas alta del mundo?", {"a) K2", "b) Everest", "c) Aconcagua"}, 'B'},
+        {"En que pais esta el Taj Mahal?",{"a) India", "b) Pakistan", "c) Nepal"}, 'A'},
+        {"Cual es el pais mas poblado del mundo?",{"a) India", "b) China", "c) Estados Unidos"}, 'B'},
+        {"Que pais tiene forma de bota?", {"a) Francia", "b) Italia", "c) Grecia"}, 'B'},
+        {"Cual es la capital de Corea del Sur?",{"a) Busan", "b) Seul", "c) Tokio"}, 'B'},
+        {"Cual es el lago mas grande de Africa?",{"a) Lago Victoria", "b) Lago Tanganica", "c) Lago Malawi"}, 'A'},
+        {"En que continente esta Surinam?", {"a) Europa", "b) Africa", "c) America"}, 'C'},
+        {"Cual es el pais mas pequeno del mundo?",{"a) Monaco", "b) San Marino", "c) Ciudad del Vaticano"}, 'C'},
+        {"Que oceano bana la costa este de Estados Unidos?",{"a) Atlantico", "b) Pacifico", "c) Indico"}, 'A'},
+        {"Cual es la capital de Nueva Zelanda?", {"a) Auckland", "b) Wellington", "c) Christchurch"}, 'B'}};
     string name;
     int salir;
     cout << "Bienvenido a nuestra trivia! Espero estes preparado para pasarla bien y divertirte contestando nuestras preguntas" << endl;
@@ -57,19 +89,19 @@ int main()
          << endl;
     do
     {
-        cout << "categorias: 1. Historia 2. Musica 3. Geografia 4. Deportes 5. Finalizar el juego" << endl;
+        cout << "categorias:\n 1. Historia\n 2. Musica\n 3. Deportes\n 4. Geografia\n 5. Finalizar el juego" << endl;
         cin >> opcion;
-        srand(time(0));
-        int numero = rand() % 7 + 1;
+        //srand(time(0));
+        //int numero = rand() % 7 + 1;
         switch (opcion)
         {
         case 1:
             cout << "usted a seleccionado historia, debes responder 7 preguntas, las cuales tendran un puntaje de 10 por cada pregunta correcta, si no es el caso se restaran 5 puntos. recuerde que sus respuestas deben ser en MAYUSCULAS" << endl;
             cout << "estas listo? Vamos!!!!" << endl;
             shuffle(preguntah.begin(), preguntah.end(), default_random_engine (time (0)));
-            for (int i = 0; i <= 7; i++)
+            for (int i = 0; i <= 6; i++)
             {
-                cout << "pregunta numero:" << i + 1 << preguntah[i].texto << endl;
+                cout << "pregunta numero: " << i + 1 <<" "<< preguntah[i].texto << endl;
 
                 for (int j = 0; j < preguntah[i].opciones.size(); j++)
                 {
@@ -85,7 +117,7 @@ int main()
                 }
                  else {
                     puntaje -= 5;
-                cout<< "su respuesta es incorrecta"<< -5 << "puntos"<<endl;
+                cout<< "su respuesta es incorrecta "<< -5 << " puntos"<<endl;
                 
             }
             }
@@ -97,9 +129,9 @@ int main()
             cout << "usted a seleccionado Musica, debes responder 7 preguntas, las cuales tendran un puntaje de 10 por cada pregunta correcta, si no es el caso se restaran 5 puntos. recuerde que sus respuestas deben ser en MAYUSCULAS " << endl;
             cout << "estas listo? Vamos!!!!" << endl;
             shuffle(preguntam.begin(), preguntam.end(), default_random_engine (time (0)));
-            for (int i = 0; i <= 7; i++)
+            for (int i = 0; i <= 6; i++)
             {
-                cout << "pregunta numero:" << i + 1 << preguntam[i].texto << endl;
+                cout << "pregunta numero: " << i + 1 <<" "<< preguntam[i].texto << endl;
 
                 for (int j = 0; j < preguntam[i].opciones.size(); j++)
                 {
@@ -115,14 +147,77 @@ int main()
                 }
                  else {
                     puntaje -= 5;
-                cout<< "su respuesta es incorrecta"<< -5 << "puntos"<<endl;
+                cout<< "su respuesta es incorrecta "<< -5 << " puntos"<<endl;
                 
             }
             }
-            cout<< "su puntuacion es:" <<puntaje<<endl;
+            cout<< "su puntuacion es: " <<puntaje<<endl;
             cout << "desea salir precione 0, si desea seguir la siguiente categoria precione 1:" << endl;
             cin >> salir;
             break;
+            case 3:
+            cout << "usted a seleccionado Deportes, debes responder 7 preguntas, las cuales tendran un puntaje de 10 por cada pregunta correcta, si no es el caso se restaran 5 puntos. recuerde que sus respuestas deben ser en MAYUSCULAS " << endl;
+            cout << "estas listo? Vamos!!!!" << endl;
+            shuffle(preguntad.begin(), preguntad.end(), default_random_engine (time (0)));
+            for (int i = 0; i <= 6; i++)
+            {
+                cout << "pregunta numero:" << i + 1 <<" "<< preguntad[i].texto << endl;
+
+                for (int j = 0; j < preguntad[i].opciones.size(); j++)
+                {
+                    cout << preguntad[i].opciones[j] << endl;
+                }
+                cin >> answer;
+
+                if (answer == preguntad[i].respuesta)
+                {
+                    cout << "usted ha seleccionado la opcion correcta ha ganado 10 puntos" << endl;
+                   puntaje += 10;
+                    
+                }
+                 else {
+                    puntaje -= 5;
+                cout<< "su respuesta es incorrecta "<< -5 << " puntos"<<endl;
+                
+            }
+            }
+            cout<< "su puntuacion es: " <<puntaje<<endl;
+            cout << "desea salir precione 0, si desea seguir la siguiente categoria precione 1:" << endl;
+            cin >> salir;
+            break;
+            case 4:
+            cout << "usted a seleccionado Geografia, debes responder 7 preguntas, las cuales tendran un puntaje de 10 por cada pregunta correcta, si no es el caso se restaran 5 puntos. recuerde que sus respuestas deben ser en MAYUSCULAS " << endl;
+            cout << "estas listo? Vamos!!!!" << endl;
+            shuffle(preguntag.begin(), preguntag.end(), default_random_engine (time (0)));
+            for (int i = 0; i <= 6; i++)
+            {
+                cout << "pregunta numero:" << i + 1 <<" "<< preguntag[i].texto << endl;
+
+                for (int j = 0; j < preguntag[i].opciones.size(); j++)
+                {
+                    cout << preguntag[i].opciones[j] << endl;
+                }
+                cin >> answer;
+
+                if (answer == preguntag[i].respuesta)
+                {
+                    cout << "usted ha seleccionado la opcion correcta ha ganado 10 puntos" << endl;
+                   puntaje += 10;
+                    
+                }
+                 else {
+                    puntaje -= 5;
+                cout<< "su respuesta es incorrecta "<< -5 << " puntos"<<endl;
+                
+            }
+            }
+            cout<< "su puntuacion es: " <<puntaje<<endl;
+            cout << "desea salir precione 0, si desea seguir la siguiente categoria precione 1:" << endl;
+            cin >> salir;
+            break;
+            cout<< "esa opcion no existe, por favor selecciones una de las opciones anteriormentes mencionadas"<<endl;
+            case 5:
+            cout<< "saliendo del programa... te esperamos de nuevo para pasarlo bien!!!"<<endl;
         default:
             break;
         }
